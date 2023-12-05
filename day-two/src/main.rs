6e6acb1e7b247d8529de7349784846933869a6a2
Aaron Ross <superhawk610@gmail.com>
2023-12-04 18:48:58 -0800

    day two, part two

--- day-two/src/main.rs
+++ day-two/src/main.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 
 fn main() {
-    part_one();
-    // part_two();
+    // part_one();
+    part_two();
 }
 
 #[allow(unused)]
@@ -27,6 +27,7 @@ fn part_one() {
 
     let game_id_sum = input
         .split("\n")
+        .map(|s| s.trim())
         .filter(|s| !s.is_empty())
         .map(parse_game)
         .filter_map(|(game_id, sets)| {
@@ -44,7 +45,37 @@ fn part_one() {
 }
 
 #[allow(unused)]
-fn part_two() {}
+fn part_two() {
+    // let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    // Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    // Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    // Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    // Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+    let input = include_str!("./input.txt");
+
+    let power_set_sum = input
+        .split("\n")
+        .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
+        .map(parse_game)
+        .map(|(game_id, sets)| {
+            sets.iter()
+                .fold(HashMap::<&str, usize>::new(), |acc, set| {
+                    set.iter().fold(acc, |mut acc, (color, count)| {
+                        acc.entry(color)
+                            .and_modify(|e| *e = std::cmp::max(*e, *count))
+                            .or_insert(*count);
+
+                        acc
+                    })
+                })
+                .into_values()
+                .product::<usize>()
+        })
+        .sum::<usize>();
+    println!("{power_set_sum}");
+}
 
 fn parse_game(line: &str) -> (usize, Vec<HashMap<&str, usize>>) {
     // remove `Game ` prefix
@@ -67,6 +98,7 @@ fn parse_game(line: &str) -> (usize, Vec<HashMap<&str, usize>>) {
                 .parse::<usize>()
                 .expect("numeric pull count");
             let color = pull.next().expect("pull color");
+            assert_eq!(pull.next(), None);
             pulls.insert(color, count);
         }
         sets.push(pulls);
